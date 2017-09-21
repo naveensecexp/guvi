45a61de81065e4a1a08d29770f21a7ac353e558c
naveensecexp <31397538+naveensecexp@users.noreply.github.com>
2017-09-21 20:49:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 s=input("Enter the string :")
-if s[0] == '-':
+if len(s) == 0:
   print(0)
+elif s[0] == '-':
+  print("negative numbers or strings not allowed")
 else:
   print("Reverse of the string ",s," is ",s[::-1])
